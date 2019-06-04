0344040c926da4568afc16dd16ca37ba724fb544
Luke <694014820@qq.com>
2019-06-04 18:09:53 +0800

    暂时想不到将补丁安全卸载的办法

---
+++ hotfix-plugins/src/main/groovy/org/keixe/android/hotfix/plugin/PackPlugin.groovy
@@ -0,0 +1,18 @@
+package org.keixe.android.hotfix.plugin
+
+import com.hujiang.gradle.plugin.android.aspectjx.AJXPlugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+final class PackPlugin implements Plugin<Project> {
+    @Override
+    void apply(Project project) {
+        if (project.plugins.hasPlugin(PatchPlugin.class)) {
+            throw new IllegalStateException("请注释或移除'apply plugin : hotfix-patch'后重试")
+        }
+        if (!project.plugins.hasPlugin(AJXPlugin.class)) {
+            project.plugins.apply(AJXPlugin.class)
+        }
+    }
+}
---
+++ hotfix-plugins/src/main/groovy/org/keixe/android/hotfix/plugin/PatchTransform.groovy
@@ -0,0 +1,81 @@
+package org.keixe.android.hotfix.plugin
+
+import com.android.build.api.transform.*
+import com.android.build.gradle.AppExtension
+import com.android.build.gradle.internal.pipeline.TransformManager
+import javassist.ClassPool
+import org.gradle.api.Project
+import org.gradle.api.logging.Logger
+
+import javax.swing.*
+import java.awt.*
+
+final class PatchTransform extends Transform {
+
+    private final String mWorkDir
+    private final Logger mLogger
+    private final ClassPool mClassPool = new ClassPool()
+
+    PatchTransform(Project project) {
+        mLogger = project.logger
+        mWorkDir = project.rootProject + File.separator + "patched" + File.separator
+        AppExtension android = project.extensions.getByType(AppExtension.class)
+        android.getBootClasspath().each { classpath ->
+            mClassPool.appendClassPath(classpath.absolutePath)
+        }
+    }
+
+    @Override
+    String getName() {
+        return getClass().getName()
+    }
+
+    @Override
+    String toString() {
+        return super.toString()
+    }
+
+    @Override
+    void transform(TransformInvocation transformInvocation) {
+        long startTime = System.currentTimeMillis()
+        mLogger.quiet "==================patched start==================="
+        transformInvocation.outputProvider.deleteAll()
+        File output = transformInvocation.outputProvider
+                .getContentLocation("main", getOutputTypes(), getScopes(), Format.DIRECTORY)
+
+        long cost = (System.currentTimeMillis() - startTime) / 1000
+        mLogger.quiet "==================patched finish=================="
+        finishWithDirectory(new File(""))
+    }
+
+    private static void finishWithDirectory(File directory) {
+        URL url = ClassLoader.systemClassLoader.getResource("./icon.png")
+        Image image = Toolkit.defaultToolkit.createImage(url)
+        image = image.getScaledInstance(40, 40, Image.SCALE_DEFAULT)
+        ImageIcon imageIcon = new ImageIcon(image)
+        JOptionPane.showConfirmDialog(null,
+                "补丁生成完成", "提示",
+                JOptionPane.DEFAULT_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                imageIcon)
+        if (Desktop.isDesktopSupported()) {
+            Desktop.desktop.open(directory)
+        }
+        System.exit(0)
+    }
+
+    @Override
+    Set<QualifiedContent.ContentType> getInputTypes() {
+        return TransformManager.CONTENT_CLASS
+    }
+
+    @Override
+    Set<? super QualifiedContent.Scope> getScopes() {
+        return TransformManager.SCOPE_FULL_PROJECT
+    }
+
+    @Override
+    boolean isIncremental() {
+        return false
+    }
+}
